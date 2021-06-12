f19730d67ed76a264f94d418c9637becd9f5109b
sabreen-parveen <sabreenparween2001@gmail.com>
2021-06-13 03:05:57 +0530

    refactor

---
+++ js/login.js
@@ -0,0 +1,58 @@
+'use strict';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { regAsBrandForm } from '../templates/registerAsBrand.js'
+
+const section1 = document.querySelector('#section--1');
+const nav = document.querySelector('.nav');
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+export const btn = document.querySelector('.btn-reg');
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+// Menu fade animation
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+// Passing "argument" into handler
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+///////////////////////////////////////
+// Sticky navigation: Intersection Observer API
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  // console.log(entry);
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
+
