1102d394114d9456a78e9ae6f6b7d251385777d2
sabreen-parveen <sabreenparween2001@gmail.com>
2021-06-17 13:40:41 +0530

    Add index file

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { LoginForm } from "../templates/login.js";
+// import { regAsBrandForm } from "../templates/registerAsBrand.js";
+// import { btn } from "../js/login.js";
+
+// ReactDOM.render(loginForm, document.getElementById("root"));
+
+// btn.addEventListener("click", () => {
+//   ReactDOM.render(regAsBrandForm, document.getElementById("root"));
+// });
