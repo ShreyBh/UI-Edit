f018f05bbec8d313cca6d06467aa370d12dca8cf
sabreen-parveen <sabreenparween2001@gmail.com>
2021-06-17 13:37:32 +0530

    Add Header Component

---
+++ components/Header.js
@@ -0,0 +1,50 @@
+import styles from "../styles/Login.module.css";
+function Header() {
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <img
+          src="/static/images/SourceqQwik_Logo_G.png"
+          alt="SourceQwik logo"
+          className={styles.nav__logo}
+          id="logo"
+          data-version-number="3.0"
+        />
+        <ul className={styles.nav__links}>
+          <li className={styles.nav__item}>
+            <a className={styles.nav__link} href="#section--1">
+              Home
+            </a>
+          </li>
+          <li className={styles.nav__item}>
+            <a className={styles.nav__link} href="#">
+              For Brands
+            </a>
+          </li>
+          <li className={styles.nav__item}>
+            <a className={styles.nav__link} href="#">
+              For Manufacturers
+            </a>
+          </li>
+          <li className={styles.nav__item}>
+            <a className={styles.nav__link} href="#">
+              Contact Us
+            </a>
+          </li>
+          <li className={styles.nav__item}>
+            <a className={`${styles.nav__link}${styles.login_box}`} href="#">
+              Log In
+            </a>
+          </li>
+          <li className={styles.nav__item}>
+            <a className={styles.nav__link} href="#">
+              #Fight Covid
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
