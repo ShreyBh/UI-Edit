9808f2a5ca01a6ed235afb123c69b4974884a643
sabreen-parveen <sabreenparween2001@gmail.com>
2021-06-17 13:37:53 +0530

    Add Layout Component

---
+++ components/Layout.js
@@ -0,0 +1,15 @@
+import Main from "./Main";
+import Footer from "./Footer";
+import Header from "./Header";
+
+function Layout(props) {
+  return (
+    <Main>
+      <Header />
+      {props.children}
+      <Footer />
+    </Main>
+  );
+}
+
+export default Layout;
