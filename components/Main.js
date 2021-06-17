7e33a7a6e68abfc6b8dfc44e1e3d6478200d6dd5
sabreen-parveen <sabreenparween2001@gmail.com>
2021-06-17 13:38:14 +0530

    Add Main Component

---
+++ components/Main.js
@@ -0,0 +1,25 @@
+function Main(props) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <link
+          rel="shortcut icon"
+          type="image/png"
+          href="img/SourceqQwik_Logo_G.png"
+        />
+
+        <link
+          href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600&display=swap"
+          rel="stylesheet"
+        />
+        <title>SourceQwik - On-Demand Manufacturing for Fashion Industry</title>
+      </head>
+      <body>{props.children}</body>
+    </html>
+  );
+}
+
+export default Main;
