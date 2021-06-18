13b7d5dfe65a6b6e5864f24a194bda21f3962e76
sabreen-parveen <sabreenparween2001@gmail.com>
2021-06-18 19:11:30 +0530

    Modify Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,35 +1,43 @@
+import styles from "../styles/Footer.module.css";
+
 function Footer() {
   return (
-    <footer className="footer">
-      <ul className="footer__nav">
-        <li className="footer__item">
-          <a className="footer__link" href="#">
+    <footer className={styles.footer}>
+      <ul className={styles.footer__nav}>
+        <li className={styles.footer__item}>
+          <a className={styles.footer__link} href="#">
             About
           </a>
         </li>
-        <li className="footer__item">
-          <a className="footer__link" href="#">
+        <li className={styles.footer__item}>
+          <a className={styles.footer__link} href="#">
             Pricing
           </a>
         </li>
-        <li className="footer__item">
-          <a className="footer__link" href="#">
+        <li className={styles.footer__item}>
+          <a className={styles.footer__link} href="#">
             Terms of Use
           </a>
         </li>
-        <li className="footer__item">
-          <a className="footer__link" href="#">
+        <li className={styles.footer__item}>
+          <a className={styles.footer__link} href="#">
             Privacy Policy
           </a>
         </li>
-        <li className="footer__item">
-          <a className="footer__link" href="#">
+        <li className={styles.footer__item}>
+          <a className={styles.footer__link} href="#">
             Contact Us
           </a>
         </li>
       </ul>
-      <img src="img/SourceqQwik_Logo_G.png" alt="Logo" class="footer__logo" />
-      <p class="footer__copyright">SourceQwik © 2021. All rights reserved</p>
+      {/* <img
+        src="../static/images/SourceqQwik_Logo_G.png"
+        alt="Logo"
+        class={styles.footer__logo}
+      /> */}
+      <p class={styles.footer__copyright}>
+        SourceQwik © 2021. All rights reserved
+      </p>
     </footer>
   );
 }
