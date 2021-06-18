4f4f518397a15f45ff7d97c3e9468184f16bfad1
sabreen-parveen <sabreenparween2001@gmail.com>
2021-06-18 19:13:54 +0530

    Add css to login page

--- pages/login/[loginAs].js
+++ pages/login/[loginAs].js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import LoginAs from "./index";
 import { useRouter } from "next/router";
+import styles from "../../styles/Form.module.css";
 
 function LoginForm() {
   const router = useRouter();
@@ -10,30 +11,39 @@ function LoginForm() {
   let registered;
   console.log(loginAs);
   if (loginAs === "brand") {
-    title = "Login as Brand";
+    title = "Login as a Brand";
     goTo = `/brand-signup`;
     registered = "Brand";
   } else {
-    title = "Login as Manufacturer";
+    title = "Login as a Manufacturer";
     goTo = `/manufacturer-signup`;
     registered = "Manufacturer";
   }
 
   return (
     <LoginAs>
-      <div className="">
-        <h2 className="login__header .section__title">{title}</h2>
-        <form className="login__form">
-          <label>Email Address</label>
-          <input type="email" />
-          <label>Password</label>
-          <input type="password" />
-          <button className="btn">Log In &rarr;</button>
+      <div className={styles.wrapper}>
+        <h2 className={`${styles.form__header}`}>{title}</h2>
+        <form>
+          <p className={styles.form__grp}>
+            <label htmlFor="email">Email Address</label>
+            <input type="email" />
+          </p>
+          <p className={styles.form__grp}>
+            <label htmlFor="password">Password</label>
+            <input type="password" />
+          </p>
+          <p className={styles.form__grp}>
+            <button className={styles.btn}>Login</button>
+          </p>
         </form>
-        <span>Not registered as a {registered}?</span>
-        <Link href="/[loginAs]" as={goTo}>
-          <button className="btn-reg">Register</button>
-        </Link>
+        <p className={styles.form__grp}>Not registered as a {registered}?</p>
+
+        <p className={styles.form__grp}>
+          <Link href="/[loginAs]" as={goTo}>
+            <button className={styles.btn}>Register</button>
+          </Link>
+        </p>
       </div>
     </LoginAs>
   );
