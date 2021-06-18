2e910a585e76970f869fa51e6fc1be561ff2c332
sabreen-parveen <sabreenparween2001@gmail.com>
2021-06-18 19:13:24 +0530

    Add css to brand-signup

--- pages/brand-signup.js
+++ pages/brand-signup.js
@@ -1,23 +1,34 @@
 import Link from "next/link";
+import styles from "../styles/Form.module.css";
 
 function BrandSignup() {
   return (
-    <section class="section" id="section--1">
-      <div class="">
-        <h2 class="login__header .section__title">Register as Brand</h2>
-        <form class="login__form">
-          <label>Brand/ Organisation Name</label>
-          <input type="email" />
-          <label>Email Address</label>
-          <input type="email" />
-          <label>Phone </label>
-          <input type="number" />
-          <button class="btn"> Register </button>
+    <section className={styles.section}>
+      <div className={styles.wrapper}>
+        <h2 className={styles.form__header}>Register as Brand</h2>
+        <form>
+          <p className={styles.form__grp}>
+            <label htmlFor="brand_name">Brand/ Organisation Name</label>
+            <input type="text" />
+          </p>
+          <p className={styles.form__grp}>
+            <label htmlFor="email">Email Address</label>
+            <input type="email" />
+          </p>
+          <p className={styles.form__grp}>
+            <label htmlFor="phone">Phone </label>
+            <input type="phone" />
+          </p>
+          <p className={styles.form__grp}>
+            <button class={styles.btn}> Register </button>
+          </p>
         </form>
-        <span>Already registerd?</span>
-        <Link href="/login/brand" passHref>
-          <button class="btn">Login</button>
-        </Link>
+        <p className={styles.form__grp}>Already registerd?</p>
+        <p className={styles.form__grp}>
+          <Link href="/login/brand" passHref>
+            <button className={styles.btn}>Login</button>
+          </Link>
+        </p>
       </div>
     </section>
   );
