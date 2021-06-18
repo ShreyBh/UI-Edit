659be2a9bdb9458aa39c6a3e2679ea9cbbee8ad6
sabreen-parveen <sabreenparween2001@gmail.com>
2021-06-18 19:14:36 +0530

    Add css to manufacturer page

--- pages/manufacturer-signup.js
+++ pages/manufacturer-signup.js
@@ -1,113 +1,146 @@
 import Link from "next/link";
+import styles from "../styles/Form.module.css";
 
 function ManufacturerSignup() {
   return (
-    <section className="section" id="section--1">
-      <div className="">
-        <h2 className="login__header .section__title">
-          Register as Manufacturer
-        </h2>
-        <form className="login__form">
-          <label>Your name</label>
-          <input type="text" />
-          <label>Firm's name</label>
-          <input type="text" />
-          <p>Your role at the firm:</p>
-          <input type="radio" id="owner" name="role" value="Owner" />
-          <label htmlFor="owner">Owner</label>
+    <section className={styles.section}>
+      <div className={styles.wrapper}>
+        <h2 className={styles.form__header}>Register as Manufacturer</h2>
+        <form>
+          <p className={styles.form__grp}>
+            <label htmlFor="manufacturer">Your name</label>
+            <input type="text" />
+          </p>
+          <p className={styles.form__grp}>
+            <label htmlFor="firm">Firm's name</label>
+            <input type="text" />
+          </p>
+          <p className={styles.form__grp}>
+            <p>Your role at the firm:</p>
+            <input type="radio" id="owner" name="role" value="Owner" />
+            <label htmlFor="owner">Owner</label>
+            <input type="radio" id="other" name="role" value="Other" />
+            <label htmlFor="other">Other</label>
+          </p>
+          <p className={styles.form__grp}>
+            <label>Website Address (if any)</label>
+            <input type="text" />
+          </p>
+          <p className={styles.form__grp}>
+            <label>Email Address</label>
+            <input type="email" />
+          </p>
+          <p className={styles.form__grp}>
+            <label>Address</label>
+            <input type="text" />
+          </p>
+          <p className={styles.form__grp}>
+            <label>Phone </label>
+            <input type="phone" />
+          </p>
+          <p className={styles.form__grp}>
+            <p>Please select your gender:</p>
+            <input type="radio" />
+            <label>Male</label>
+            <input type="radio" />
+            <label>Female</label>
+          </p>
+          <p className={styles.form__grp}>
+            <p></p>
+            <input type="checkbox" />
+            <label>Men's wear</label>
+            <input type="checkbox" />
+            <label>Ladies wear</label>
 
-          <input type="radio" id="other" name="role" value="Other" />
-          <label htmlFor="other">Other</label>
+            <input type="checkbox" />
+            <label>Kid's wear</label>
 
-          <label>Website Address (if any)</label>
-          <input type="text" />
-          <label>Email Address</label>
-          <input type="email" />
-          <label>Address</label>
-          <input type="text" />
-          <label>Phone </label>
-          <input type="number" />
-          <p>Please select your gender:</p>
-          <input type="checkbox" />
-          <label>Men's wear</label>
+            <input type="checkbox" />
+            <label>Home</label>
 
-          <input type="checkbox" />
-          <label>Ladies wear</label>
+            <input type="checkbox" />
+            <label>Accessoories</label>
 
-          <input type="checkbox" />
-          <label>Kid's wear</label>
+            <input type="checkbox" />
+            <label>Others</label>
+          </p>
+          <p className={styles.form__grp}>
+            <label>What do you manufacture in above categories?</label>
+            <input type="text" />
+          </p>
+          <p className={styles.form__grp}>
+            <label>How many pieces can you make in day? *</label>
+            <input type="number" />
+          </p>
+          <p>
+            <p className={styles.form__grp}>Are you Sedex certified :</p>
+            <input type="radio" />
+            <label>Yes</label>
+            <input type="radio" />
+            <label>No</label>
+            <input type="radio" />
+            <label>Maybe</label>
+          </p>
+          <p className={styles.form__grp}>
+            <p>Do you make your own designs</p>
+            <input type="radio" />
+            <label>Yes</label>
 
-          <input type="checkbox" />
-          <label>Home</label>
+            <input type="radio" />
+            <label>No</label>
+          </p>
+          <p className={styles.form__grp}>
+            <p>Do you procure your own raw materials?</p>
+            <label>Yes</label>
 
-          <input type="checkbox" />
-          <label>Accessoories</label>
+            <input type="radio" />
+            <label>No</label>
 
-          <input type="checkbox" />
-          <label>Others</label>
+            <input type="radio" />
+            <label>Maybe</label>
+          </p>
+          <p className={styles.form__grp}>
+            <p>Do you have your own QA/QC team?</p>
+            <input type="radio" />
+            <label>Yes</label>
 
-          <label>What do you manufacture in above categories?</label>
-          <input type="text" />
-          <label>How many pieces can you make in day? *</label>
-          <p>Are you Sedex certified :</p>
-          <input type="radio" />
-          <label>Yes</label>
-
-          <input type="radio" />
-          <label>No</label>
-
-          <input type="radio" />
-          <label>Maybe</label>
-
-          <p>Do you make your own designs</p>
-          <input type="radio" />
-          <label>Yes</label>
-
-          <input type="radio" />
-          <label>No</label>
-
-          <p>Do you procure your own raw materials?</p>
-          <label>Yes</label>
-
-          <input type="radio" />
-          <label>No</label>
-
-          <input type="radio" />
-          <label>Maybe</label>
-
-          <p>Do you have your own QA/QC team?</p>
-          <input type="radio" />
-          <label>Yes</label>
-
-          <input type="radio" />
-          <label>No</label>
-
-          <p>Tell us about some brands you have worked with?</p>
-          <textarea id="subject" name="subject"></textarea>
-          <div id="drop-area">
-            {/* <div className="my-form">
-              <p>
-                Upload multiple files with the file dialog or by dragging and
-                dropping images onto the dashed region
-              </p>
-              <input
-                type="file"
-                id="fileElem"
-                multiple
-                accept="image/*"
-                onchange="handleFiles(this.files)"
-              />
-              <label className="button" htmlFor="fileElem">
-                Select some files
-              </label>
-            </div> */}
-          </div>
-          <button className="btn"> Submit </button>
+            <input type="radio" />
+            <label>No</label>
+          </p>
+          <p className={styles.form__grp}>
+            <p>Tell us about some brands you have worked with?</p>
+            <textarea id="subject" name="subject"></textarea>
+          </p>
+          <p className={styles.form__grp}>
+            <div id="drop-area">
+              <div>
+                <p>
+                  Upload multiple files with the file dialog or by dragging and
+                  dropping images onto the dashed region
+                </p>
+                <input
+                  type="file"
+                  id="fileElem"
+                  multiple
+                  accept="image/*"
+                  onchange="handleFiles(this.files)"
+                />
+                <label className="button" htmlFor="fileElem">
+                  Select some files
+                </label>
+              </div>
+            </div>
+          </p>
+          <p className={styles.form__grp}>
+            <button className={styles.btn}> Register </button>
+          </p>
         </form>
-        <p>Already registerd?</p>
-        <Link href="/login/manufacturer" passHref>
-          <button className="btn">Login</button>
-        </Link>
+        <div className={styles.form__grp}>
+          <p>Already registerd?</p>
+          <Link href="/login/manufacturer" passHref>
+            <button className={styles.btn}>Login</button>
+          </Link>
+        </div>
       </div>
     </section>
   );
